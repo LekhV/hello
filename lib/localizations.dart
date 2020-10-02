4aa3886d21841088619ed923dd90f9866779b3da
Dev <dev@Devs-Mac-mini.local>
2020-10-02 14:35:00 +0300

    add l10n

---
+++ lib/localizations.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
+
+import 'l10n/messages_all.dart';
+
+class AppLocalizations {
+  static Future<AppLocalizations> load(Locale locale) {
+    final String name = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
+
+    final localeName = Intl.canonicalizedLocale(name);
+
+    return initializeMessages(localeName).then((bool _) {
+      Intl.defaultLocale = localeName;
+
+      return AppLocalizations();
+    });
+  }
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  String get title {
+    return Intl.message(
+      'This is my first localization App',
+      name: 'title',
+    );
+  }
+}
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ru'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return AppLocalizations.load(locale);
+  }
+
+  @override
+  bool shouldReload(AppLocalizationsDelegate old) {
+    return false;
+  }
+}
